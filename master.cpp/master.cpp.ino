306c621be1a16cd8e1dfab471fd59da6d74f45a9
satiowadahc <woitas.chad@gmail.com>
2018-11-25 15:51:50 -0600

    CoAP Protocol psuedo written
    TODO rewrite report, add in flow sensor

--- master.cpp/master.cpp.ino
+++ master.cpp/master.cpp.ino
@@ -4,71 +4,59 @@
 
 // DISPLAY / SERVER NODE
 
-// TODO Count Delays
-// Add in signal chart
 // improve servo message
-
 
 //#include <RadioHead.h>
 #include <SPI.h>
 #include <RH_RF95.h>
+#include <RHReliableDatagram.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 #include <Adafruit_STMPE610.h>
 #include <Wire.h>
 
+
+// Touch Screen
 #define TFT_CS   9
 #define TFT_DC   10
 #define STMPE_CS 6
 #define SD_CS    5
-
-TS_Point p, op;
-int ry, rx;
-
 #define TS_MINX 3800
 #define TS_MAXX 100
 #define TS_MINY 100
 #define TS_MAXY 3750
-
 #define BOXSIZE 80
-
-RH_RF95 rf95(8, 7);
+TS_Point p, op;
+int ry, rx;
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 Adafruit_STMPE610 ts = Adafruit_STMPE610(STMPE_CS);
 
-int led = 13;
+//Radio
+#define CLIENT_ADDRESS 2
+#define SERVER_ADDRESS 1
+RH_RF95 rf95(8, 7);
+RHReliableDatagram manager(rf95, SERVER_ADDRESS);
+uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 
-//Display info for connection status
+
+////Display info for connection status
 bool conFlag = false;
 int count = 0;
 
-
-
-void setup() {
-  delay(50);
-  pinMode(led, OUTPUT);     
+void setup() {   
   
   tft.begin();
-
-  Serial.begin(9600);
   
-  if (!rf95.init()){
-    tft.println("Radio init failed");
-  }
-  if (!ts.begin()){
-    tft.println("Touch init failed");
-  }  
+  if (!ts.begin())     tft.println("Touch init failed");  
+  if (!manager.init()) tft.println("Proto init failed");
   
-  tft.setRotation(3);
-
+  tft.setRotation(3);//USB FACING DOWN
   tft.fillScreen(ILI9341_BLACK);
-  //TODO COLOUR SCHEME
   tft.fillRect(160, 0, BOXSIZE, BOXSIZE, ILI9341_RED);
   tft.fillRect(160+BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_YELLOW);
   tft.fillRect(160, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_ORANGE);
   tft.fillRect(160+BOXSIZE, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_BLUE);
   tft.fillRect(160, BOXSIZE*2, BOXSIZE*2, BOXSIZE, ILI9341_GREEN);
-
   tft.setTextColor(ILI9341_WHITE);
   tft.setTextSize(4);
   tft.setCursor(180, 25);
@@ -81,59 +69,57 @@ void setup() {
   tft.println("Off");
   tft.setCursor(180, 25+BOXSIZE*2);
   tft.println("Pump");   
-  
-  tft.setTextColor(ILI9341_RED); 
+  tft.setTextColor(ILI9341_BLACK); 
   tft.setTextSize(2);
 }
 
 void loop() {
 
-  // TOUCH STUFF
+  // PUT ------------------------------------------------------------
   op = p;
   p = ts.getPoint(); 
   rx = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
   ry = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
   if(p!=op){
-    Serial.print(rx);
-    Serial.print(",");
-    Serial.println(ry);
     if(0 < rx && rx < BOXSIZE){
+      tft.setTextColor(ILI9341_WHITE);
+      tft.setTextSize(4);
       if(ry < BOXSIZE){
+          tft.fillRect(160+BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
           uint8_t data[] = "s2";
-          rf95.send(data, sizeof(data));
-          rf95.waitPacketSent();
-          Serial.println("servo2");
-          delay(50);
+          manager.sendtoWait(data, sizeof(data), CLIENT_ADDRESS);
+          tft.fillRect(160+BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_YELLOW);
+          tft.setCursor(260, 25);
+          tft.println("T-");
+          tft.fillRect(0,220,160,30,ILI9341_GREEN);
+          tft.setTextSize(2);
+          tft.setCursor(2,225);
+          tft.print("T- Request Sent");
+          yield();
       }
       if(BOXSIZE < ry && ry < BOXSIZE*2){
+        tft.fillRect(160, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
         uint8_t data[] = "s1";
-        rf95.send(data, sizeof(data));
-        rf95.waitPacketSent();
-        Serial.println("servo1");
-        delay(50);
+        manager.sendtoWait(data, sizeof(data), CLIENT_ADDRESS);
+        tft.fillRect(160, 0, BOXSIZE, BOXSIZE, ILI9341_RED);
+        tft.setCursor(180, 25);
+        tft.println("T+");
+        tft.fillRect(0,220,160,30,ILI9341_GREEN);
+        tft.setTextColor(ILI9341_BLACK); 
+        tft.setTextSize(2);
+        tft.setCursor(2,225);
+        tft.print("T+ Request Sent");
+        yield();
       }
     }
   }
 
-
   // Radio stuff
-
-  //I Don't think available is the proper function here
-  if (rf95.available()){
-    // Should be a message for us now   
-    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    uint8_t len = sizeof(buf);
-
-    uint8_t tbuf[RH_RF95_MAX_MESSAGE_LEN];
-    uint8_t tlen = sizeof(buf);
-    
-    if (rf95.recv(buf, &len)){
-      Serial.println("---");
-      for(int i = 0; i <20; i++){
-        Serial.println(buf[i]);
-      }
-
-      //Data Reading
+  if (manager.available()){
+      uint8_t len = sizeof(buf);
+      uint8_t from;
+    if (manager.recvfromAck(buf, &len, &from)){
+      //GET ----------------------------------------------
       if(buf[0] == 115){
         //st
         if(buf[1] == 116){
@@ -141,6 +127,7 @@ void loop() {
           tft.setCursor(2,5);
           tft.print("Well Temp: ");
           tft.println(buf[3]);
+          yield();
         } 
         //sh
         else if (buf[1] == 108){
@@ -148,54 +135,56 @@ void loop() {
           tft.setCursor(2,30);
           tft.print("Humidity: ");
           tft.println(buf[3]);
+          yield();
         }
         //sf
         else if (buf[1] == 102){
+          //TODO CONNECT SENSOR
           tft.fillRect(0,50,160,25,ILI9341_WHITE);
           tft.setCursor(2,55);
           tft.print("Flow: ");
           tft.println(buf[3]);
+          yield();
         }
-      }//End Data Readings
-
-      //Status readings
-      //cxx
-      if(buf[0] == 99){
-        //c01
-        if(buf[1] == 48 && buf[2] == 49){
+        uint8_t data[] = "cc02";
+        manager.sendtoWait(data, sizeof(data), from);
+        tft.fillRect(0,220,160,30,ILI9341_GREEN);
+        tft.setCursor(2,225);
+        tft.print("Data RX");
+        yield();
+        conFlag = true;
+        count = 0;
+      
+      }//END DATA SWITCH
+      else if(buf[0] == 99 && buf[1] == 99){
+        //c01 - Acknowledge Message received
+        if(buf[2] == 48 && buf[2] == 49){
           tft.fillRect(0,220,160,30,ILI9341_GREEN);
           tft.setCursor(2,225);
           tft.print("Connected");
+          yield();
           conFlag = true;
           count = 0;
         }
+      } //End connection switch
+      else{
+        //403 - Bad request
+        uint8_t data[] = "cc03";
+        manager.sendtoWait(data, sizeof(data), from);
       }
-      // Ack
-      uint8_t data[] = "c00";
-      rf95.send(data, sizeof(data));
-      rf95.waitPacketSent();
-      
-      delay(50);
-    }
-    else
-    {
-      tft.println("recv failed");
-    }   
-  } // End Radios
-
-  delay(100);
-  if(conFlag){
-    conFlag = false;
-    count=0;
-  }
-  else{
+     }//End Message Readings
+   }
+   else{
     count++;
-    if(count%20==0){
-    tft.fillRect(0,225,160,30,ILI9341_GREEN);
-    tft.setTextColor(ILI9341_RED);
-    tft.setCursor(2,225);
-    tft.print(count);
-    tft.setTextColor(ILI9341_BLACK);
-    }
+   }
+
+  if(count%20 == 1 && !conFlag){
+      tft.fillRect(0,220,160,30,ILI9341_GREEN);
+      tft.setCursor(2,225);
+      tft.print("Timer:");
+      tft.print(count-1);
+      yield();
   }
+  conFlag = false;
+  delay(500);
 } // end loop
--- node.cc/node.cc.ino
+++ node.cc/node.cc.ino
@@ -18,45 +18,50 @@
 // c01 - ack send
 
 #include <SPI.h>
+#include <RHReliableDatagram.h>
 #include <RH_RF95.h>
 #include <Adafruit_PWMServoDriver.h>
 
-
+// Radio -------------------------------------------------
 RH_RF95 rf95(8, 7);
+#define CLIENT_ADDRESS 2
+#define SERVER_ADDRESS 1
+RHReliableDatagram manager(rf95, CLIENT_ADDRESS);
+uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 
+// Controls ----------------------------------------------
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-
 uint8_t s1[3] = {115, 49, 0}; //s1
 uint8_t s2[3] = {115, 50, 0}; //s2
-
 #define SERVOMIN  150 // tune
 #define SERVOMAX  450 // tune
 int servoPosition = 1;
 
+// Sensors  ----------------------------------------------
 #define DHTTYPE DHT22
-
 DHT dht(18, DHTTYPE);
 
 int led =13;
+
 void setup() {
   Serial.begin(9600);
+  while(!Serial);
   pinMode(led, OUTPUT); 
-  if (!rf95.init())
-    Serial.println("init failed");
-  //TODO add in blink code
-
-  //TODO Check for errrs?
+//  if (!rf95.init())   Serial.println("Radio init failed");
+  if(!manager.init()) Serial.println("Proto init failed");
   pwm.begin();
   pwm.setPWMFreq(60); 
-
   dht.begin();
 }
 
-void loop() {
+void loop(){
+
+  uint8_t len = sizeof(buf);
+  uint8_t from; 
+
   //Ack Packets
   uint8_t ack[] = "c01";
-  rf95.send(ack, sizeof(ack));
-  rf95.waitPacketSent();
+  manager.sendtoWait(ack, sizeof(ack), SERVER_ADDRESS);
   //End Ack Packets
 
   //Data Readings
@@ -69,58 +74,64 @@ void loop() {
   
   char tdata = temp;
   uint8_t stdata[4] = {115, 116, 0, tdata}; 
-  rf95.send(stdata, sizeof(stdata));
-  rf95.waitPacketSent();
+  if(manager.sendtoWait(stdata, sizeof(stdata), SERVER_ADDRESS)){
+      Serial.print("Temp Sent: ");
+      Serial.println(temp);
+    if (manager.recvfromAckTimeout(buf, &len, 2000, &from)){
+      Serial.print("got reply from : 0x");
+      Serial.print(from, HEX);
+      Serial.print(": ");
+      Serial.println((char*)buf);
+    }
+    else{
+      Serial.println("No reply, is rf95_reliable_datagram_server running?");
+    }
+  }
   
   char hdata = humid;
   uint8_t shdata[4] = {115, 108, 0, hdata};
-  rf95.send(shdata, sizeof(shdata));
-  rf95.waitPacketSent();
+  if(manager.sendtoWait(shdata, sizeof(shdata), SERVER_ADDRESS)){
+    if (manager.recvfromAckTimeout(buf, &len, 2000, &from)){
+      Serial.print("Humid Sent: ");
+      Serial.println(humid);
+      Serial.print("got reply from : 0x");
+      Serial.print(from, HEX);
+      Serial.print(": ");
+      Serial.println((char*)buf);
+    }
+    else{
+      Serial.println("No reply, is rf95_reliable_datagram_server running?");
+    }    
+  }
   
-  //END DATA READINGS
-  
-  // Now wait for a reply
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
+  //END DATA READINGS 
 
-  if (rf95.waitAvailableTimeout(3000))
-  { 
-    // Should be a reply message for us now   
-    if (rf95.recv(buf, &len))
-   {
+  if(manager.available()){
+  if(manager.recvfromAckTimeout(buf, &len, 3000, &from)){ 
       Serial.print("got reply: ");
       Serial.println((char*)buf);
-      signalBlink(len); 
+      signalBlink(3); 
       if(buf[0]==s1[0] && buf[1]==s1[1] && servoPosition==2){
         for(uint16_t i = SERVOMIN; i < SERVOMAX; i++){
           pwm.setPWM(0,0,i);
         }
           servoPosition = 1;
-          uint8_t data[] = "Servo 1 Triggered";
-          rf95.send(data, sizeof(data));
-          rf95.waitPacketSent();
+          uint8_t data[] = "s001";
+          manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS);
       }
       if(buf[0]==s2[0] && buf[1]==s2[1] && servoPosition==1){
         for(uint16_t i = SERVOMAX; i > SERVOMIN; i--){
           pwm.setPWM(0,0,i);
         } 
           servoPosition = 2;       
-          uint8_t data[] = "Servo 2 Triggered";
-          rf95.send(data, sizeof(data));
-          rf95.waitPacketSent();
+          uint8_t data[] = "s002";
+          manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS);
       }
     }
-    else
-    {
-      Serial.println("recv failed");
-    }
   }
-  else
-  {
-    Serial.println("No reply, is rf95_server running?");
-    errorBlink();
-  }
-}
+  
+  delay(100); //for viewing purposes
+}// end loop
 
 
 void errorBlink(){
